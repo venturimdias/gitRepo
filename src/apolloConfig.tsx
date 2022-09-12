210b411ad5ed616de1f4792bba0eac765238521e
Jonathan Venturim <venturim.dias@gmail.com>
2022-09-12 11:51:37 -0300

    ajuste apollo config

--- src/apolloConfig.tsx
+++ src/apolloConfig.tsx
@@ -9,7 +9,7 @@ const apolloConfig = () => {
     return {
       headers: {
         ...headers,
-        authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN || process.env.github_token}`,
+        authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN || process.env.GITHUB_TOKEN}`,
       }
     }
   })
