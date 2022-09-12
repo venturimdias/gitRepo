b2789699f6dc1a6da35f49173e018c5ca86d6602
Jonathan Venturim <venturim.dias@gmail.com>
2022-09-12 10:39:08 -0300

    correção de errors

--- src/components/Repo/Repo.tsx
+++ src/components/Repo/Repo.tsx
@@ -90,7 +90,7 @@ const Repo = ({ perfil }: { perfil?: string }) => {
       return i
     })
     return topics
-  }, [filterTopic, listData])
+  }, [filterTopic, data?.user.repositories.edges])
 
   useEffect(() => {
     setList(listData)
